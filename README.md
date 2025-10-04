329d7cfcc3b157be4cf5dcc0aec909eb83ae7a04
Marcel Berger <marcelb1504@gmail.com>
2025-10-04 16:25:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Homepage Backend
+
+Spring boot backend of my personal homepage.
+
+## Versions
+
+- Spring Boot: 3.5.6
+- Java: 25
---
+++ src/main/java/me/marcelberger/homepage/backend/HpBackendApplication.java
@@ -0,0 +1,17 @@
+package me.marcelberger.homepage.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "me.marcelberger")
+@EntityScan("me.marcelberger")
+@EnableJpaRepositories("me.marcelberger")
+public class HpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HpBackendApplication.class, args);
+    }
+
+}
